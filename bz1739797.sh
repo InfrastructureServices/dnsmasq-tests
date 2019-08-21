7520a06cdf946cf7922ea1eda22774c29f1a473b
Petr Menšík <pemensik@redhat.com>
2019-08-21 17:06:29 +0200

    Also terminate started dnsmasq
    Let it run for some time, then display output of journal

--- bz1739797.sh
+++ bz1739797.sh
@@ -3,9 +3,20 @@
 #
 # Reproducer for https://bugzilla.redhat.com/show_bug.cgi?id=1739797
 
+TIMEOUT=${1:-30}
+
 CODEDIR="`(dirname -- "$0")`"
 source $CODEDIR/settings.conf
 source $CODEDIR/setup.sh
 
+echo starting
 # Run joint DHCP4/DHCP6 server with router advertisement enabled in veth namespace
 $IN_NS dnsmasq --pid-file=/tmp/dhcp_$BRDEV.pid --dhcp-leasefile=/tmp/dhcp_$BRDEV.lease --dhcp-range=${IPV4_PREFIX}.10,${IPV4_PREFIX}.254,240 --dhcp-range=${IPV6_PREFIX}::10,${IPV6_PREFIX}::1ff,slaac,64,240 --enable-ra --interface=$BRDEV --bind-interfaces
+
+echo waiting
+sleep $TIMEOUT
+
+echo terminating
+kill $(cat /tmp/dhcp_$BRDEV.pid)
+
+journalctl -xe

833c189a80682b83d13bf0ffadc23de6151a5c79
Petr Menšík <pemensik@redhat.com>
2019-08-21 18:57:17 +0200

    Fix format date

--- bz1739797.sh
+++ bz1739797.sh
@@ -4,13 +4,13 @@
 # Reproducer for https://bugzilla.redhat.com/show_bug.cgi?id=1739797
 
 TIMEOUT=${1:-30}
-LINES=100
+MAXLINES=100
 
 CODEDIR="`(dirname -- "$0")`"
 source $CODEDIR/settings.conf
 source $CODEDIR/setup.sh
 
-START=$(date -Iminutes | tr T ' ')
+START=$(date '+%Y-%m-%d %H:%M:%S')
 echo starting
 # Run joint DHCP4/DHCP6 server with router advertisement enabled in veth namespace
 $IN_NS dnsmasq --pid-file=/tmp/dhcp_$BRDEV.pid --dhcp-leasefile=/tmp/dhcp_$BRDEV.lease --dhcp-range=${IPV4_PREFIX}.10,${IPV4_PREFIX}.254,240 --dhcp-range=${IPV6_PREFIX}::10,${IPV6_PREFIX}::1ff,slaac,64,240 --enable-ra --interface=$BRDEV --bind-interfaces
@@ -21,7 +21,7 @@ sleep $TIMEOUT
 echo terminating
 kill $(cat /tmp/dhcp_$BRDEV.pid)
 
-COUNT=$(journalctl -exn $LINES -t 'dnsmasq' -t 'dnsmasq-dhcp' -S "$START" --no-pager | grep "RTR-ADVERT($BRDEV)" | wc -l)
+COUNT=$(journalctl -exn $MAXLINES -t 'dnsmasq' -t 'dnsmasq-dhcp' -S "$START" --no-pager | grep "RTR-ADVERT($BRDEV)" | wc -l)
 
 clean
 
